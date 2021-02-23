7b9790f8ecbb9d1effa2cbe0d1bbc71859d7ea06
Tom O'Donnell <dev.todonnell@gmail.com>
2021-02-23 16:06:16 -0500

    Added Answers Page
    The quiz simply said if you were correct or not. Now it will show
    information about the quiz word after the user responds.

--- middle_kingdom/quiz.py
+++ middle_kingdom/quiz.py
@@ -9,30 +9,28 @@ bp = Blueprint("quiz", __name__, url_prefix="/quiz")
 
 
 @bp.route("/", methods=("GET", "POST"))
-def index():
-    if request.method == "POST":
-        answer = request.form["answer"]
-        is_correct = "Correct!" if answer == session["word"] else "Wrong!"
-        flash(is_correct)
-
-    db = get_db()
-    word = db.execute(
-        "SELECT simplified FROM words ORDER BY RANDOM() LIMIT 1"
-    ).fetchone()["simplified"]
-    session["word"] = word
-    return render_template("quiz/quiz.html", word=word)
-
 @bp.route("/hsk<int:level>", methods=("GET", "POST"))
-def hsk(level):
+def index(level=None):
+    db = get_db()
     if request.method == "POST":
         answer = request.form["answer"]
-        is_correct = "Correct!" if answer == session["word"] else "Wrong!"
+        is_correct = "Correct!" if answer == session["word"]["simplified"] else "Wrong!"
         flash(is_correct)
+        word_info = db.execute(
+            "SELECT * FROM words WHERE id = ?",
+            (session["word"]["id"],)
+        ).fetchone()
+        return render_template("quiz/answer.html", word_info=dict(word_info))
 
-    db = get_db()
-    word = db.execute(
-        "SELECT simplified FROM words WHERE hsk_level = ? ORDER BY RANDOM() LIMIT 1",
-        (level,)
-    ).fetchone()["simplified"]
-    session["word"] = word
+    if level:
+        word = db.execute(
+            "SELECT id, simplified FROM words WHERE hsk_level = ? ORDER BY RANDOM() LIMIT 1",
+            (level,)
+        ).fetchone()
+    else:
+        word = db.execute(
+            "SELECT id, simplified FROM words ORDER BY RANDOM() LIMIT 1"
+        ).fetchone()
+
+    session["word"] = dict(word)
     return render_template("quiz/quiz.html", word=word)
